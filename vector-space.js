19e7a19be253dcd70dd5ddb593bf1e6809be3e97
khai96_ <hvksmr1996@gmail.com>
2016-03-10 10:07:57 +0700

    update idea

--- vector-space.js
+++ vector-space.js
@@ -3,7 +3,42 @@
 	'use strict';
 
 	class VectorSpace extends require('x-vector/vector-space') {
-		
+
+		constructor() {
+
+			class MatrixBase extends this.Vector {
+
+				createTransposition() {
+					return new TransMatrix(this);
+				}
+
+				static createMatrixProduct(left, right) {
+					return new ProductMatrix(left, right);
+				}
+
+			}
+
+			class Matrix extends MatrixBase {
+
+			}
+
+			class TransMatrix extends MatrixBase {
+
+				constructor(base) {
+					this.base = base;
+				}
+
+			}
+
+			class ProductMatrix extends MatrixBase {}
+
+			return {
+				Matrix: Matrix,
+				__proto__: this
+			};
+
+		}
+
 	}
 
 	module.exports = class extends VectorSpace {};
